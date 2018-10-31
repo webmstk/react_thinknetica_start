78331371734eb12ab341341baf09e94e3409066c
webmstk <webmstk@mail.ru>
2018-10-31 16:26:17 +0300

    add HelloWorld

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 import Calculator from '~/src/Calculator'
+import HelloWorld from '~/src/HelloWorld'
 
 const a = 6
 const b = 2
@@ -8,3 +11,8 @@ console.log(`${a} + ${b} =`, calc.plus(a, b))
 console.log(`${a} - ${b} =`, calc.minus(a, b))
 console.log(`${a} * ${b} =`, calc.multiply(a, b))
 console.log(`${a} / ${b} =`, calc.divide(a, b))
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.getElementById('root')
+)
