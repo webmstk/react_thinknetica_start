663c1130189dee4512ff3537218cdbf4ba1346a1
webmstk <webmstk@mail.ru>
2018-11-01 18:33:04 +0300

    tab 2 spaces in CalculatorDemo

--- src/CalculatorDemo.js
+++ src/CalculatorDemo.js
@@ -5,19 +5,19 @@ import Calculator from '~/src/Calculator'
 const calc = new Calculator()
 
 const CalculatorDemo = ({ a, b }) => {
-    return (
-        <div>
-            <p>{a} + {b} = {calc.plus(a, b)}</p>
-            <p>{a} - {b} = {calc.minus(a, b)}</p>
-            <p>{a} * {b} = {calc.multiply(a, b)}</p>
-            <p>{a} / {b} = {calc.divide(a, b)}</p>
-        </div>
-    )
+  return (
+    <div>
+      <p>{a} + {b} = {calc.plus(a, b)}</p>
+      <p>{a} - {b} = {calc.minus(a, b)}</p>
+      <p>{a} * {b} = {calc.multiply(a, b)}</p>
+      <p>{a} / {b} = {calc.divide(a, b)}</p>
+    </div>
+  )
 }
 
 CalculatorDemo.propTypes = {
-    a: PropTypes.number.isRequired,
-    b: PropTypes.number.isRequired,
+  a: PropTypes.number.isRequired,
+  b: PropTypes.number.isRequired,
 }
 
 export default CalculatorDemo
